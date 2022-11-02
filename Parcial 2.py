593133d8fd1456bf0929f73f9fdb4bafc561dbe5
maxiicba <40280687+maxiicba@users.noreply.github.com>
2022-11-02 10:45:40 -0300

    Solo falta el ultimo punto

--- Parcial 2.py
+++ Parcial 2.py
@@ -36,44 +36,53 @@ while i <= cantidad:
     cargar_Pelicula()
     i += 1 """
 
-peliculas[12] = {"Genero":1,"Descripcion":"Scream 4","Cantidad":4,"Alquiladas":4,"Estado":2}
+peliculas[233] = {"Genero":1,"Descripcion":"Scream 4","Cantidad":2,"Alquiladas":4,"Estado":2}
 peliculas[13] = {"Genero":2,"Descripcion":"Padre de familia","Cantidad":42,"Alquiladas":2,"Estado":1}
-peliculas[144] = {"Genero":1,"Descripcion":"Dragon ball Z","Cantidad":7,"Alquiladas":6,"Estado":3}
-peliculas[233] = {"Genero":3,"Descripcion":"Black mirror","Cantidad":4,"Alquiladas":6,"Estado":1}
+peliculas[144] = {"Genero":2,"Descripcion":"Dragon ball Z","Cantidad":7,"Alquiladas":6,"Estado":3}
+peliculas[2] = {"Genero":3,"Descripcion":"Black mirror","Cantidad":4,"Alquiladas":6,"Estado":1}
+
+def peliculasGeneroMayor():
+    for clave1 in genero:
+        valor = genero[clave1]      
+        generosCount = [] 
+        for clave in peliculas:
+            generosCount.append(peliculas[clave]["Genero"])
+    print ("El genero que mas se alquila es: " + genero[max(set(generosCount), key=generosCount.count)])
+    print("================================")
 
 def peliculasGenero():
-    print("================================")
     for clave1 in genero:
         valor = genero[clave1]
         generosCount = [] 
         for clave in peliculas:
             generosCount.append(peliculas[clave]["Genero"])
-        print("En el genero " + str(valor) + " hay: " + str(generosCount.count(clave1)) + " peliculas")
+    print("En el genero " + str(valor) + " hay: " + str(generosCount.count(clave1)) + " peliculas")
+    print("================================")
     
 def copiasAlquiladas():
-    print("================================")
     total = 0
     for clave in peliculas:
         valor = peliculas[clave]
         total = total + valor["Alquiladas"]
     print("Total de copias alquiladas: ", total)
+    print("================================")
 
 def peliculasXdesc():
-    print("================================")
     array = []
     for key in peliculas:
         array.append(peliculas[key]["Descripcion"])
     ordenados = sorted(array)
     print("Lista de peliculas ordenadas por descripción:")
     print(ordenados)
+    print("================================")
 
 def peliculasEliminar():
-    print("================================")
     print(bcolors.FAIL +"Peliculas a eliminar:" + bcolors.RESET)
     for clave2 in peliculas:
         valor = peliculas[clave2]
         if int(valor["Cantidad"]) <= int(valor["Alquiladas"]):
             print(peliculas[clave2]["Descripcion"])
+    print("================================")
 
 def peliculaCodigo():
     codigo=int(input(bcolors.WARNING +"Ingrese el codigo para buscar una pelicula:" + bcolors.RESET))
@@ -82,14 +91,6 @@ def peliculaCodigo():
     else:
      print(bcolors.FAIL +'Esta clave no existe' + bcolors.RESET)
 
-def peliculasGeneroMayor():
-    print("================================")
-    for clave1 in genero:
-        valor = genero[clave1]
-        generosCount = [] 
-        for clave in peliculas:
-            generosCount.append(peliculas[clave]["Genero"])
-    print ("GENERO:" + genero[max(generosCount)])
 
 def listarPelis():
     print("Peliculas del genero 1 y 2:")
@@ -99,6 +100,41 @@ def listarPelis():
             print(peliculas[clave2])
     print("================================")
 
+def listarPorCodigo():
+    keys = peliculas.keys()
+    sorted_keys = sorted(keys)
+    sorted_desserts = {}
+    for key in sorted_keys:
+        sorted_desserts[key] = peliculas[key]
+    print(bcolors.OK +'Lista de peliculas ordenadas por código' + bcolors.RESET)
+    print(sorted_desserts)
+    print("================================")
+
+def buscarPorDes():
+    descripcion=input(bcolors.WARNING +"Ingrese la descripcion para buscar una pelicula: " + bcolors.RESET)
+    for key in peliculas:
+        valor = peliculas[key]
+        if valor["Descripcion"] == descripcion:
+            print(bcolors.OK + "Resultado de la busqueda:" + bcolors.RESET)
+            print(valor)
+        else:
+            print(bcolors.FAIL +'No se encontraron resultados con este criterio de busqueda (' + descripcion + ")" + bcolors.RESET)
+            break
+
+def listarCopiasInferior():
+    for key in peliculas:
+        valor = peliculas[key]
+        if int(valor["Cantidad"]) <= 3:
+            print(bcolors.OK + "Listado de peliculas menor a 3 copias:" + bcolors.RESET)
+            print(valor)
+            break
+        else:
+            print(bcolors.FAIL + 'No se encontraron peliculas menor a 3 copias' + bcolors.RESET)
+            break
+
+listarCopiasInferior()
+buscarPorDes()
+listarPorCodigo()
 listarPelis()
 peliculasGeneroMayor()
 peliculasEliminar()
